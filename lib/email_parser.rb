ca8351b0468c62a4f60a11a4e21276b0298e7b51
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-31 16:20:41 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,8 +7,8 @@ class EmailAddressParser
   
   attr_accessor :csv_emails
   
-  def initialize(csv_email)
-    @csv_emails = csv_email
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
 end
 
 def parse
